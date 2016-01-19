46aa0552bf06df903083ff1851ac41a8ec129348
ansarimofid <ansarimofid@gmail.com>
2016-01-19 22:51:00 +0530

    Changed Directory structure

---
+++ app.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+
+console.log("Hello World");
+/*var fs = require("fs");
+var path = process.cwd() + "/test";
+fs.mkdir(path, function(error) {
+    if (error) {
+        console.error("mkdir error: " + error.message);
+    } else {
+        path += "/test2";
+        fs.mkdir(path, function(error) {
+            if (error) {
+                console.error("mkdir error: " + error.message);
+            } else {
+                console.log("Successfully built " + path);
+            }
+        });
+    }
+});
+*/
