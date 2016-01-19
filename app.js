016d4d8ce0b227e3f8d8780f2596545d3eab5505
ansarimofid <ansarimofid@gmail.com>
2016-01-20 02:00:16 +0530

    Complete directory Structure for Simple Html Project can be Created

--- app.js
+++ app.js
@@ -1,28 +1,59 @@
 #!/usr/bin/env node
 
+var fs = require("fs-extra");
+
 var arg = process.argv.slice(2)[0];
 console.log("Argument:" + arg);
 
 
 if (arg == 'simpleHtmlProject') {
     createSimpleHtmlProject();
-}
-else
+} else
     return 0;
 
 
+/**
+ * Creates Directory For Simple Html5 Project
+ * @return {boolean} return 1 on success else 0
+ */
 function createSimpleHtmlProject() {
-    var fs = require("fs-extra"),
-        currPath = process.cwd();
-    mkDir('css',currPath);
-    mkDir('img',currPath);
-    mkDir('js',currPath);
+    var currPath = process.cwd(),
+        path = __dirname;
+    mkDir('css', currPath);
+    mkDir('img', currPath);
+    mkDir('js', currPath);
+    copyFile(path + '/lib/template/html/index.html', currPath + '/index.html');
+    copyFile(path + '/lib/template/css/style.css', currPath + '/css/style.css');
+    copyFile(path + '/lib/template/js/main.js', currPath + '/js/main.js');
 }
 
-function mkDir(dirName,path){
-    var fs = require("fs");
-    fs.mkdir(path+'/'+dirName, function(err){
-        if (err){
+
+/**
+ * Creates Directory us FileSystem
+ * @param  {string} dirName Name of The Directory to be Created
+ * @param  {string} path    Path of root directory
+ * @return {bool}         returns 1 on sucsess else 0
+ */
+function mkDir(dirName, path) {
+    fs.mkdir(path + '/' + dirName, function(err) {
+        if (err) {
+            console.log(err);
+            return 0;
+        }
+        return 1;
+    });
+}
+
+
+/**
+ * Copies file or directory from source to destination
+ * @param  {string} src source of file/directory
+ * @param  {string} dst destination of new file/directory
+ * @return {bool}     returns 1 on success else 1
+ */
+function copyFile(src, dst) {
+    fs.copy(src, dst, function(err) {
+        if (err) {
             console.log(err);
             return 0;
         }
